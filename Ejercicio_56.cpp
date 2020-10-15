b60445157444cc218ba626f7bc658f158c31bb8a
Esquivel Soto Jesus Francisco <71353268+EsquivelJesus@users.noreply.github.com>
2020-10-15 14:54:09 -0700

    Update Ejercicio_56.cpp

--- Ejercicio_56.cpp
+++ Ejercicio_56.cpp
@@ -1,30 +1,21 @@
-// Ejercicios_Lenguajes.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
-//
+
 
 #include <iostream>
 using namespace std;
 
 int main()
 {
-	int Calificaciones[20];
-	int contador = 0;
-	for (int x = 0; x < 20; x++) {
+	int Calificaciones[20]; //Iiniciza un arreglo para 20 datos de tipo integer
+	int contador = 0; //Inicializa una variable de tipo integer
+	for (int x = 0; x < 20; x++) {  //Ciclo for para capturar los 20 datos
 		cout << "Ingrese la calificacion del examnen " << x + 1 << " \n";
 		cin >> Calificaciones[x];
-		if (Calificaciones[x] < 65)
+		if (Calificaciones[x] < 65) //separa las calificaciones que se contaran para su despliegue
 			contador++;
 	}
-	cout << "El numero de examenes menor a 65 son: " << contador << endl ;
+	cout << "El numero de examenes menor a 65 son: " << contador << endl ;  //Despliega el numero de examenes menores a 65
 
 	return 0;
 }
 
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
 
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln

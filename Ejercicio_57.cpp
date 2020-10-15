375f669370494d7a9c579e580b7f7ce81f2fcdbf
Esquivel Soto Jesus Francisco <71353268+EsquivelJesus@users.noreply.github.com>
2020-10-15 15:09:23 -0700

    Update Ejercicio_57.cpp

--- Ejercicio_57.cpp
+++ Ejercicio_57.cpp
@@ -5,19 +5,19 @@ using namespace std;
 
 int main()
 {
-	int N = 0, acumulador = 0, Numero = 0;
-	double Promedio = 0;
+	int N = 0, acumulador = 0, Numero = 0; //Declaracion de las variables de tipo INTEGER para acumular y recibir el numero
+	double Promedio = 0; //Declaracion de la variable donde se almacenara el promedio
 
-	cout << "¿Cuantos numeros desea ingresar? \n";
+	cout << "¿Cuantos numeros desea ingresar? \n"; //Se pide el numero de datos que se capturaran
 	cin >> N;
 
-	for (int x = 0; x < N; x++) {
+	for (int x = 0; x < N; x++) {  //Ciclo para capturar el dato numerico y acumularlo de igual manera 
 		cout << "Ingrese el numero " << x + 1 << "\n";
 		cin >> Numero;
 		acumulador += Numero;
 	}
-	Promedio = acumulador / N;
-	cout << "El promedio es: " << Promedio << endl;
+	Promedio = acumulador / N;  //Se calcula el promedio despues de terminar el ciclo
+	cout << "El promedio es: " << Promedio << endl;  //Despliegue del resultado
 
 	return 0;
 }

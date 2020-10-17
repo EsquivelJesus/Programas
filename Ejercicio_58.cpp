29b6202e9f5621acaeff5e5e743003c7da8b815c
Esquivel Soto Jesus Francisco <71353268+EsquivelJesus@users.noreply.github.com>
2020-10-17 13:28:20 -0700

    Update Ejercicio_58.cpp

--- Ejercicio_58.cpp
+++ Ejercicio_58.cpp
@@ -5,11 +5,12 @@ using namespace std;
 
 int main()
 {
-	int Numero = 0, Res = 0, NumeroInvertido = 0;  //Declaracion de las tres variables de tio INTEGER Del numero a capturar el residuo y en donde se pondra el valor invertido
+	int Numero = 0, Res = 0, NumeroInvertido = 0,numanterior = 0;  //Declaracion de las cuatro variables de tio INTEGER Del numero a capturar el residuo y en donde se pondra el valor invertido
 	
 	//se captura el valor a invertir
 	cout << "Ingrese el numero: "; 
 	cin >> Numero;
+	numanterior = Numero; //Guardamos la variable para compararla con el resultado
 
 	//Ciclo para sacar el residuo de cifra para asi invertirlo y agruegarlo en la otra variable
 	while (Numero > 0) {
@@ -17,7 +18,7 @@ int main()
 		Numero /= 10;
 		NumeroInvertido = NumeroInvertido * 10 + Res;
 	}
-	cout << "EL INVERSO DE  " << Numero << " es " << NumeroInvertido << endl; //Despliegue del valor ingresado y el valor invertido
+	cout << "EL INVERSO DE  " << numanterior << " es " << NumeroInvertido << endl; //Despliegue del valor ingresado y el valor invertido
 
 
 	return 0;
